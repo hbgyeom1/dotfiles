9bd99242fe52929a69dc9b3d6c574bba53addece
hbgyeom1 <hbgyeom@gmail.com>
2025-05-15 13:54:09 +0900

    1

--- Scripts/overwrite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -r ~/Dotfiles/Overwrites/.user.zsh ~/.user.zsh
-cp -r ~/Dotfiles/Overwrites/.config/* ~/.config/
-cp -r ~/Dotfiles/Overwrites/.local/* ~/.local/
--- Scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ -d ~/Desktop ] && rm -rf ~/Desktop
-[ -d ~/Music ] && rm -rf ~/Music
-[ -d ~/Public ] && rm -rf ~/Public
-[ -d ~/Templates ] && rm -rf ~/Templates
-[ -d ~/Videos ] && rm -rf ~/Videos
