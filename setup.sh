6a048c6a6ae6eda5c7519b6271424cbc88452208
hbgyeom <151603203+hbgyeom1@users.noreply.github.com>
2025-05-24 21:45:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
-
 mkdir -p ~/.local/share/applications
 cp /usr/share/applications/obsidian.desktop ~/.local/share/applications/obsidian.desktop
