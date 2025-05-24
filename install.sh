9cf1b2a96277fdd442ba91084369cb3fd910338e
hbgyeom <151603203+hbgyeom1@users.noreply.github.com>
2025-05-24 20:43:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 sudo pacman -S --needed \
-  neovim zellij yazi lazygit \
-  fcitx5-hangul fcitx5-gtk fcitx5-configtool \
-  docker libreoffice-fresh obsidian
+    neovim zellij lazygit \
+    fcitx5-hangul fcitx5-gtk fcitx5-configtool \
+    docker libreoffice-fresh obsidian
